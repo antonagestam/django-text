fb1e5b519e820d1fef7330c6b828513afe6a1130
Anton Agestam <msn@antonagestam.se>
2015-02-24 02:20:12 +0100

    fix

--- text/static/text/js/markdown-widget.js
+++ text/static/text/js/markdown-widget.js
@@ -1,15 +1,13 @@
 (function ($) {
     "use strict";
     $(function () {
-        $(".markdown").hide().each(function () {
-            var markDownEl = this;
-            new MediumEditor(document.querySelector(".editor"), {
-                extensions: {
-                    markdown: new MeMarkdown(function (md) {
-                        markDownEl.val(md);
-                    })
-                }
-            });
+        var markDownEl = $(".markdown").hide();
+        new MediumEditor($(".editor").get(), {
+            extensions: {
+                markdown: new MeMarkdown(function (md) {
+                    markDownEl.val(md);
+                })
+            }
         });
     });
 }(django.jQuery));
