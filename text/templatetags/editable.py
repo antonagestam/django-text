bcc70f78ee29b33766f84315ebe727171460b07b
Anton Agestam <msn@antonagestam.se>
2015-02-24 03:13:10 +0100

    try to achieve one query per request

--- text/templatetags/editable.py
+++ text/templatetags/editable.py
@@ -1,3 +1,5 @@
+from sets import Set
+
 from django import template
 
 from ..models import Text
@@ -5,16 +7,40 @@ from ..models import Text
 register = template.Library()
 
 
+class TextGetter(object):
+    def __init__(self):
+        self.registered_texts = Set()
+        self.texts = {}
+
+    def register(self, text_name):
+        self.registered_texts.add(text_name)
+
+    def require(self, text_name):
+        if text_name not in self.registered_texts:
+            self.register(text_name)
+        self.get_registered_texts()
+        return self.texts[text_name]
+
+    def get_registered_texts(self):
+        texts = Text.objects.filter(name__in=self.registered_texts)
+        for text in texts:
+            self.texts[text.name] = text
+
+
+text_getter = TextGetter()
+
+
 class TextNode(template.Node):
     def __init__(self, text_name):
         self.text_name = text_name
+        text_getter.register(text_name)
 
     def render(self, context):
         try:
-            text = Text.objects.get(name=self.text_name)
-        except Text.DoesNotExist:
+            text = text_getter.require(self.text_name).render()
+        except KeyError:
             text = self.text_name
-        return text.render()
+        return text
 
 
 @register.tag(name='editable')
