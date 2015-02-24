35218c06657c1eabc8d5dde15d71d16b8661be70
Anton Agestam <msn@antonagestam.se>
2015-02-24 19:09:14 +0100

    blockeditable w/ default text

--- text/templatetags/editable.py
+++ text/templatetags/editable.py
@@ -18,13 +18,35 @@ class TextNode(template.Node):
         return text
 
 
-@register.tag(name='editable')
-def do_editable(parser, token):
+class BlockTextNode(TextNode):
+    def __init__(self, nodelist, *args, **kwargs):
+        super(BlockTextNode, self).__init__(*args, **kwargs)
+        self.nodelist = nodelist
+
+    def render(self, context):
+        try:
+            text = text_getter.require(self.text_name).render()
+        except KeyError:
+            text = self.nodelist.render(context)
+        return text
+
+
+def get_text_name(token):
     try:
         # split_contents() knows not to split quoted strings.
-        tag_name, text_name = token.split_contents()
+        __, text_name = token.split_contents()
     except ValueError:
         raise template.TemplateSyntaxError(
             "%r tag requires a single argument" % token.contents.split()[0])
+    return text_name
 
-    return TextNode(text_name)
+@register.tag(name='editable')
+def editable(parser, token):
+    return TextNode(get_text_name(token))
+
+
+@register.tag(name='blockeditable')
+def blockeditable(parser, token):
+    nodelist = parser.parse(('endblockeditable', ))
+    parser.delete_first_token()
+    return BlockTextNode(nodelist, get_text_name(token))
