023cc9c3f3195fcdcf5f3c7a2b07aa7ed5f27b15
Anton Agestam <msn@antonagestam.se>
2015-02-25 20:00:41 +0100

    fix

--- text/middleware.py
+++ text/middleware.py
@@ -5,3 +5,6 @@ class AutoPopulateMiddleware(object):
     def process_response(self, request, response):
         text_setter.save()
         return response
+
+    def process_request(self, request):
+        text_setter.clear()
