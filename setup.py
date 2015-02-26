97da2d333bd5399a94e103091a90a0c209bdecdf
Anton Agestam <msn@antonagestam.se>
2015-02-26 18:44:12 +0100

    fix, add migration

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages
 setup(
     name='django-text',
     description='Intuitive text editing for the Django Admin.',
-    version='1.2.5',
+    version='1.2.6',
     long_description=open('README.md').read(),
     author='Anton Agestam',
     author_email='msn@antonagestam.se',
