a0930c85fe4eaa3dbd561cbf9ebe91a33ca1b506
Anton Agestam <msn@antonagestam.se>
2015-03-07 16:36:04 +0100

    instantly updates DOM with saved text

--- text/conf.py
+++ text/conf.py
@@ -1,9 +1,13 @@
 from django.conf import settings as django_settings
 
 
-class Conf:
+class Conf(object):
     AUTOPOPULATE_TEXT = getattr(django_settings, 'AUTOPOPULATE_TEXT', True)
     TOOLBAR_FORM_PREFIX = getattr(django_settings, 'TEXT_TOOLBAR_FORM_PREFIX', 'djtext_form')
     TOOLBAR_ENABLED = getattr(django_settings, 'TEXT_TOOLBAR_ENABLED', True)
+    TOOLBAR_INSTANT_UPDATE = getattr(django_settings, 'TEXT_TOOLBAR_INSTANT_UPDATE', True)
+    INLINE_WRAPPER = getattr(django_settings, 'TEXT_INLINE_WRAPPER', (
+        '<span data-text-name="{0}" class="{1}">', '</span>'))
+    INLINE_WRAPPER_CLASS = getattr(django_settings, 'TEXT_INLINE_WRAPPER_CLASS', 'dj_text_inline_wrapper')
 
 settings = Conf()
