ff2f319f805accd31fbe48fe2230fec5a3432e60
Anton Agestam <msn@antonagestam.se>
2015-12-21 00:27:09 +0100

    minor cleanup

--- text/conf.py
+++ text/conf.py
@@ -1,13 +1,16 @@
+from functools import partial
+
 from django.conf import settings as django_settings
+
+s = partial(getattr, django_settings)
 
 
 class Conf(object):
-    AUTOPOPULATE_TEXT = getattr(django_settings, 'AUTOPOPULATE_TEXT', True)
-    TOOLBAR_FORM_PREFIX = getattr(django_settings, 'TEXT_TOOLBAR_FORM_PREFIX', 'djtext_form')
-    TOOLBAR_ENABLED = getattr(django_settings, 'TEXT_TOOLBAR_ENABLED', True)
-    TOOLBAR_INSTANT_UPDATE = getattr(django_settings, 'TEXT_TOOLBAR_INSTANT_UPDATE', True)
-    INLINE_WRAPPER = getattr(django_settings, 'TEXT_INLINE_WRAPPER', (
-        '<span data-text-name="{0}" class="{1}">', '</span>'))
-    INLINE_WRAPPER_CLASS = getattr(django_settings, 'TEXT_INLINE_WRAPPER_CLASS', 'dj_text_inline_wrapper')
+    AUTOPOPULATE_TEXT = s('AUTOPOPULATE_TEXT', True)
+    TOOLBAR_FORM_PREFIX = s('TEXT_TOOLBAR_FORM_PREFIX', 'djtext_form')
+    TOOLBAR_ENABLED = s('TEXT_TOOLBAR_ENABLED', True)
+    TOOLBAR_INSTANT_UPDATE = s('TEXT_TOOLBAR_INSTANT_UPDATE', True)
+    INLINE_WRAPPER = s('TEXT_INLINE_WRAPPER', ('<span data-text-name="{0}" class="{1}">', '</span>'))
+    INLINE_WRAPPER_CLASS = s('TEXT_INLINE_WRAPPER_CLASS', 'dj_text_inline_wrapper')
 
 settings = Conf()
