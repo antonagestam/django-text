267252ef7514739ae8b6b7950f0549fe7a3115ba
derek73 <derek73@users.noreply.github.com>
2016-03-13 15:35:38 -0700

    accept dashes from language ID in urls
    urls like `/django_text/text/text_body_en-us/` have a dash in them. Previous regex did not allow accept it.

--- text/urls.py
+++ text/urls.py
@@ -5,6 +5,6 @@ from .views import TextView, TextUpdateView
 
 urlpatterns = patterns(
     '',
-    url(r'^text/(?P<text_slug>\w+)/$', TextView.as_view(), name='text'),
+    url(r'^text/(?P<text_slug>[\w-]+)/$', TextView.as_view(), name='text'),
     url(r'^update_text/(?P<text_id>\d+)/$', TextUpdateView.as_view(), name='update_text'),
 )
