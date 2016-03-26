850acbed2fd12005a840a33e84c6c9862c9edb5a
Anton Agestam <msn@antonagestam.se>
2016-03-26 01:19:36 +0100

    fix mising textarea update

--- text/static/text/js/html-widget.js
+++ text/static/text/js/html-widget.js
@@ -1,26 +1,33 @@
 (function ($) {
     "use strict";
 
-    function init_editor(editor) {
-        new MediumEditor(editor.get(), {
+    function get_editor(editor_element, textarea) {
+        var editor = new MediumEditor(editor_element.get(), {
             firstHeader: 'h1',
             secondHeader: 'h2'
         });
+
+        editor.subscribe('editableInput', function (event, editable) {
+            textarea.val(editor.serialize()['element-0'].value);
+        });
+
+        return editor;
     }
 
     $(function () {
         var textarea = $(".djtext_editor_input"),
-            editor = $(".djtext_html_editor"),
-            type_select = editor.parents('form').find('[id$="type"]');
+            editor_element = $(".djtext_html_editor"),
+            type_select = editor_element.parents('form').find('[id$="type"]'),
+            editor;
 
         function set_mode(mode) {
             if (mode == 'html') {
                 textarea.hide();
-                editor.show();
-                init_editor(editor);
+                editor_element.show();
+                editor = get_editor(editor_element, textarea);
             } else {
                 textarea.show();
-                editor.hide();
+                editor_element.hide();
             }
         }
 
